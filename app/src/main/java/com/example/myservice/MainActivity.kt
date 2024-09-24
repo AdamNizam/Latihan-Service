678527515b0cca9ad0db8ab5967d475ffd475a6d
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-09-24 22:28:31 +0700

    foregroundService

--- app/src/main/java/com/example/myservice/MainActivity.kt
+++ app/src/main/java/com/example/myservice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myservice
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,12 +19,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main){ v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val additionalPadding = resources.getDimensionPixelSize(R.dimen.additional_padding)
+
+            v.setPadding(
+                systemBars.left + additionalPadding,
+                systemBars.top + additionalPadding,
+                systemBars.right + additionalPadding,
+                systemBars.bottom + additionalPadding
+            )
+
             insets
         }
+
 
         val serviceIntent = Intent(this, MyBackgroundService::class.java)
 
@@ -35,6 +44,20 @@ class MainActivity : AppCompatActivity() {
         // menghentikan service
         binding.btnStopBackgroundService.setOnClickListener {
             stopService(serviceIntent)
+        }
+
+        // kelas foreground service
+
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
         }
 
     }
